ea23807208102a5b639c67a62ca6b55a53221487
NureAlamShuvo <nurealamshuvo11@gmail.com>
2026-03-31 16:30:53 +0600

    add tap switching Products and Cart section

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,10 +1,12 @@
-import { Check } from 'lucide-react';
-import React, { use } from 'react';
 
-const Tools = ( {toolsPromise} ) => {
+import React, { use, useState } from 'react';
+import ToolsCard from './ToolsCard';
+
+const Tools = ({ toolsPromise }) => {
 
     const tools = use(toolsPromise);
-    console.log(tools);
+
+    const [activeTap, setActiveTap] = useState("products");
 
     return (
         <div className='py-28'>
@@ -13,32 +15,26 @@ const Tools = ( {toolsPromise} ) => {
                 <p className='text-[#627382]'>Choose from our curated collection of premium digital products designed <br />to boost your productivity and creativity.</p>
 
                 <div className='flex gap-4 justify-center'>
-                    <button className='btn btn-primary'>Products</button>
-                    <button className='btn btn-outline'>Products</button>
+                    <button onClick={() => setActiveTap("products")} className={`btn ${activeTap === "products" ? "btn-primary" : "btn-outline"}`}>Products</button>
+
+                    <button onClick={() => setActiveTap("cart")} className={`btn ${activeTap === "cart" ? "btn-primary" : "btn-outline"}`}>Cart</button>
+
                 </div>
             </div>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 container mx-auto mt-10'>
-                {
-                    tools.map(tool => <div key={tool.id} className='rounded-2xl bg-[#F2F2F2] p-10 space-y-4'>
-                        <img src={tool.img} alt="" />
-                        <h2 className='font-bold text-[1.2rem] text-[#101727]'>{tool.name}</h2>
-                        <p className='text-[#627382]'>{tool.description}</p>
-                        <h2> <span className='font-bold text-[1.2rem] text-[#101727]'>${tool.price}</span><span className='text-[#627382]'>/{tool.period}</span></h2>
-                        <div className='space-y-2'>
-                            {
-                                tool.features.map((feature, index) => (
-                                    <p key={index} className='flex items-center gap-2'>
-                                    <span className='text-green-400'><Check size={16} /></span> 
-                                    <span className='text-[#627382]'>{feature}</span>
-                                </p>)
-                                )
-                            }
-                        </div>
-                        <button className='btn btn-primary w-full'>Buy Now</button>
-                    </div>)
-                }
-            </div>
+            {
+                activeTap === "products" && (
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 container mx-auto mt-10'>
+                        {
+                            tools.map(tool =>
+                                <ToolsCard key={tool.id} tool={tool}></ToolsCard>)
+                        }
+                    </div>
+                )
+            }
+
+            {activeTap === "cart" && <cart></cart>}
+            
         </div>
     );
 };
---
+++ src/components/ToolsCard.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Check } from 'lucide-react';
+
+const ToolsCard = ({ tool }) => {
+
+    const [clickBtn, setClickBtn] = useState(false);
+
+    const handleBuyBtn = () =>{
+        setClickBtn(true);
+    }
+
+    return (
+        <div className='rounded-2xl bg-[#F2F2F2] p-10 space-y-4'>
+
+                            <img src={tool.img} alt="Image not found!" />
+
+                            <h2 className='font-bold text-[1.2rem] text-[#101727]'>{tool.name}</h2>
+
+                            <p className='text-[#627382]'>{tool.description}</p>
+
+                            <h2> <span className='font-bold text-[1.2rem] text-[#101727]'>${tool.price}</span><span className='text-[#627382]'>/{tool.period}</span></h2>
+                            <div className='space-y-2'>
+                                
+                                {
+                                    tool.features.map((feature, index) => (
+                                        <p key={index} className='flex items-center gap-2'>
+                                            <span className='text-green-400'><Check size={16} /></span>
+                                            <span className='text-[#627382]'>{feature}</span>
+                                        </p>)
+                                    )
+                                }
+                            </div>
+                            <button onClick={handleBuyBtn} className={`btn w-full ${clickBtn ? "btn-success" : "btn-primary"}`}>{clickBtn ? <span className='flex items-center gap-2'><Check size={16}/>Added To Card</span> : "Buy Now"}</button>
+                        </div>
+    );
+};
+
+export default ToolsCard;
